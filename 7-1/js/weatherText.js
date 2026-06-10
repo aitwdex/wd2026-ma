85846d510be74f86c05c270f60e8c4851b83bc65
伊藤 真叶 <bluerose.mi.0710@gmail.com>
2026-06-10 15:21:31 +0900

    Delete 7-1 directory

--- 7-1/js/weatherText.js
+++
@@ -1,9 +0,0 @@
-export function codeToText(code) {
-  if (code === 0) return "快晴 ☀️";
-  if (code <= 3) return "晴れ〜曇り ⛅";
-  if (code <= 48) return "霧 🌫️";
-  if (code <= 67) return "雨 🌧️";
-  if (code <= 77) return "雪 ❄️";
-  if (code <= 82) return "にわか雨 🌦️";
-  return "雷雨 ⛈️";
-}
